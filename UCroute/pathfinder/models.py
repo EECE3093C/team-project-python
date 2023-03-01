a0bc2235d4e1cc5a9d46b84f74e14804924802ec
jordanshaheen <jordan.shaheen@icloud.com>
2023-03-01 15:09:15 -0500

    updated model fields, added obstruction input for users, initalized css styling templates for webpages and favicon

--- UCroute/pathfinder/models.py
+++ UCroute/pathfinder/models.py
@@ -3,18 +3,19 @@ from django.db import models
 
 # Create your models here.
 class Path(models.Model):
-    starting_point = models.CharField(max_length=100)
-    end_point = models.CharField(max_length=100)
-    distance = models.CharField(max_length=100)
+    starting_point = models.CharField(max_length=100, default='')
+    end_point = models.CharField(max_length=100, default='')
+    distance = models.CharField(max_length=100, default='')
     handicapped = models.BooleanField(default=False)
+    obstructed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.starting_point
 
 
 class Building(models.Model):
-    name = models.CharField(max_length=200)
-    location = models.CharField(max_length=100)
+    name = models.CharField(max_length=200, default='')
+    location = models.CharField(max_length=100, default='')
 
     def __str__(self):
         return self.name
@@ -22,6 +23,7 @@ class Building(models.Model):
 
 class Entrances(models.Model):
     building = models.ForeignKey(Building, on_delete=models.CASCADE, default=None)
-    location = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, default='')
     handicapped = models.BooleanField(default=False)
+    obstructed = models.BooleanField(default=False)
 
--- UCroute/pathfinder/urls.py
+++ UCroute/pathfinder/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('findroute/', views.routefinder, name='routefinder'),
+    path('obstruction/', views.obstruction, name='obstruction'),
     path('result/', views.result, name='result'),
+    # path('<str:start>/<str:end>/result, views.result, name='result')
 ]
+
